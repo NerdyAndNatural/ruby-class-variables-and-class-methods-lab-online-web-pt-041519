17e944bef86b0454d30a33cbb9985e00d15c17d3
Sarah Williams <srwilliams8678@gmail.com>
2019-05-15 00:50:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,5 +59,5 @@ mask_off = Song.new("Mask Off", "Future", "Rap")
 dance_macabre = Song.new("Dance Macabre", "Ghost", "Metal")
 if_you_have_ghosts = Song.new("If You Have Ghosts", "Ghost", "Metal")
 Song.genres
-p Song.artist_count
-p Song.genre_count
+Song.artist_count
+Song.genre_count
